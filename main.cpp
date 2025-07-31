9be7523dd21064a923f91942f91af71063b724f6
Hollow1125 <aleksej_nesterov-09@bk.ru>
2025-07-31 22:33:10 +0500

    Добавил итерацию по папкам

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <opencv2/dnn.hpp>
 #include <iostream>
 #include <vector>
-
+#include <filesystem>
 
 const cv::String keys =
     "{help h        | | Print this message}"
@@ -10,8 +10,11 @@ const cv::String keys =
     "{eastModel e    | | Path to EAST model}"
     "{dbModel d      | | Path to DB model}";
 
-void DB50TextDetection(cv::Mat image, cv::String &modelPath);
-void EASTTextDetection(cv::Mat image, cv::String &modelPath);
+void DB50TextDetection(cv::Mat image, cv::String &modelPath, std::string& outputPath);
+void EASTTextDetection(cv::Mat image, cv::String &modelPath, std::string& outputPath);
+void ProbabilisticHoughTransform(cv::Mat image, cv::String &modelPath);
+void HoughTransform(cv::Mat image, cv::String &modelPath);
+void CheckDirectoryExists(std::filesystem::path &directory);
 
 int main(int argc, char** argv)
 {
@@ -31,51 +34,131 @@ int main(int argc, char** argv)
         parser.printErrors();
         return 1;
     }
-    
 
     cv::String imagePath = parser.get<cv::String>("inputImage");
+    cv::String eastModelPath = parser.get<cv::String>("eastModel");
+    cv::String dbModelPath = parser.get<cv::String>("dbModel");
+
     if (imagePath.empty())
     {
-        std::cout << "Error: No input image specified" << std::endl;
+        std::wcout << L"Ошибка чтения изображения" << std::endl;
         return 1;
     }
 
-    cv::Mat image = cv::imread(imagePath);
-    if(image.empty())
+    std::filesystem::path pathToImages(imagePath);
+    std::filesystem::path outputDirEAST = pathToImages / "EAST";
+    std::filesystem::path outputDirDB = pathToImages / "DB50";
+    
+    try 
     {
-        std::cout << L"Ошибка загрузки изображения" << std::endl;
+        std::filesystem::create_directory(outputDirEAST);
+        std::filesystem::create_directory(outputDirDB);
+    } 
+    catch (const std::filesystem::filesystem_error& e) 
+    {
+        std::wcerr << L"Ошибка создания каталога" << e.what() << std::endl;
         return 1;
     }
 
-    //! Probabilistic HoughTransofrm
+    try
+    {
+        for (const auto& i : std::filesystem::recursive_directory_iterator(pathToImages))
+        {
+            if (i.is_regular_file())
+            {
+                std::string extension = i.path().extension().string();
+                if (extension == ".png" || extension == ".PNG" || extension == ".jpeg" || extension == ".JPEG" || extension == ".jpg" || extension == ".JPG")
+                {
+                    cv::Mat image = cv::imread(i.path().string());
+                    if(image.empty())
+                    {
+                        std::cout << L"Ошибка загрузки изображения" << std::endl;
+                        continue;
+                    }
+                    
+                    std::filesystem::path relativePath = std::filesystem::relative(i.path(), pathToImages);
+                    std::filesystem::path parentDir = relativePath.parent_path();
+
+                    std::filesystem::path eastOutputPath = outputDirEAST / parentDir / i.path().filename();
+                    std::filesystem::path dbOutputPath = outputDirDB / parentDir / i.path().filename();
+
+                    CheckDirectoryExists(eastOutputPath.parent_path());
+                    CheckDirectoryExists(dbOutputPath.parent_path());
+
+                    //ProbabilisticHoughTransform(image);
+                    //HoughTransform(image);
+
+                    if (!eastModelPath.empty())
+                    {
+                        EASTTextDetection(image.clone(), eastModelPath, eastOutputPath.string());
+                    }
+                    else
+                    {
+                        std::cout << "No path for EAST model have been provided" << std::endl;
+                    }
+                    if (!dbModelPath.empty())
+                    {
+                        DB50TextDetection(image.clone(), dbModelPath, dbOutputPath.string());
+                    }
+                    else
+                    {
+                        std::cout << "No path for DB50 model have been provided" << std::endl;
+                    }
+                }
+            }
+        }
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+
+    return 0;
+}
+
+void CheckDirectoryExists(std::filesystem::path &directory)
+{
+    if (!std::filesystem::exists(directory)) 
+    {
+        std::filesystem::create_directories(directory);
+    }
+}
+
+void ProbabilisticHoughTransform(cv::Mat image)
+{
     cv::Mat HoughLinesPImage = image.clone();
     //Mat resized, upscaled;
     //pyrDown(HoughLinesPImage, resized, Size(image.cols/2, image.rows/2));
     //pyrUp(resized, upscaled, image.size());
     cv::Mat gray;
-    cvtColor(HoughLinesPImage, gray, cv::COLOR_BGR2GRAY);
     cv::Mat edges;
+    cvtColor(HoughLinesPImage, gray, cv::COLOR_BGR2GRAY);
+    //imwrite("edges.jpg", edges);
     cv::Canny(gray, edges, 50, 150, 3);
-
-    //* Black and white edges
-    imwrite("edges.jpg", edges);
 
     std::vector<cv::Vec4i> lines;
     HoughLinesP(edges, lines, 1, CV_PI/180, 50, 20, 10);
-
-    for(size_t i = 0; i < lines.size(); i++) {
+    for(size_t i = 0; i < lines.size(); i++) 
+    {
         cv::Vec4i l = lines[i];
         cv::line(HoughLinesPImage, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), cv::Scalar(0, 255, 0), 2);
     }
-    imwrite("houghlinesP.jpg", HoughLinesPImage);
 
-    //! Regular HoughTransofrm
+    imwrite("houghlinesP.jpg", HoughLinesPImage);
+}
+
+void HoughTransform(cv::Mat image)
+{
     cv::Mat HoughLinesImage = image.clone();
     std::vector<cv::Vec2f> linesP;
+
+    cv::Mat edges;
     cv::HoughLines(edges, linesP, 1, CV_PI / 180, 200);  
 
     // Draw the lines
-    for (int i = 0; i < linesP.size(); i++) {
+    for (int i = 0; i < linesP.size(); i++)
+    {
         float rho = linesP[i][0];
         float theta = linesP[i][1];
         cv::Point pt1, pt2;
@@ -93,31 +176,9 @@ int main(int argc, char** argv)
     }
 
     imwrite("HoughLines.jpg", HoughLinesImage);
-
-    
-    cv::String eastModelPath = parser.get<cv::String>("eastModel");
-    cv::String dbModelPath = parser.get<cv::String>("dbModel");
-    if (!eastModelPath.empty())
-    {
-        EASTTextDetection(image, eastModelPath);
-    }
-    else
-    {
-        std::cout << "No path for EAST model have been provided" << std::endl;
-    }
-    if (!dbModelPath.empty())
-    {
-        DB50TextDetection(image, dbModelPath);
-    }
-    else
-    {
-        std::cout << "No path for DB50 model have been provided" << std::endl;
-    }
-
-    return 0;
 }
 
-void EASTTextDetection(cv::Mat image, cv::String &modelPath)
+void EASTTextDetection(cv::Mat image, cv::String &modelPath, std::string& outputPath)
 {
     try
     {
@@ -138,21 +199,20 @@ void EASTTextDetection(cv::Mat image, cv::String &modelPath)
         std::vector<std::vector<cv::Point>> detResults;
         model.detect(image, detResults);
 
-        cv::Mat visualization = image.clone();
         bool rectangleIsClosed = true;
         int lineThickness = 2;
         for (const auto& polygon : detResults) {
-            cv::polylines(visualization, polygon, rectangleIsClosed, cv::Scalar(0, 255, 0), lineThickness);
+            cv::polylines(image, polygon, rectangleIsClosed, cv::Scalar(0, 255, 0), lineThickness);
         }
-        imwrite("east_detection.jpg", visualization);
+        imwrite(outputPath, image);
     }
     catch (const cv::Exception& e) 
     {
-        std::cerr << "EAST Text Detection Error: " << e.what() << std::endl;
+        std::wcerr << L"Ошибка: " << e.what() << std::endl;
     }
 }
 
-void DB50TextDetection(cv::Mat image, cv::String& modelPath) 
+void DB50TextDetection(cv::Mat image, cv::String& modelPath, std::string& outputPath) 
 {
     try {
         cv::dnn::TextDetectionModel_DB model(modelPath);
@@ -174,14 +234,13 @@ void DB50TextDetection(cv::Mat image, cv::String& modelPath)
         std::vector<std::vector<cv::Point>> detResults;
         model.detect(image, detResults);
 
-        cv::Mat visualization = image.clone();
         for (const auto& polygon : detResults) {
-            cv::polylines(visualization, polygon, true, cv::Scalar(0, 255, 0), 2);
+            cv::polylines(image, polygon, true, cv::Scalar(0, 255, 0), 2);
         }
-        cv::imwrite("db_detection.jpg", visualization);
+        cv::imwrite(outputPath, image);
     } 
     catch (const cv::Exception& e) 
     {
-        std::cerr << "DB Text Detection Error: " << e.what() << std::endl;
+        std::wcerr << L"Ошибка: " << e.what() << std::endl;
     }
 }
